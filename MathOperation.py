fdc2ae64c33aed18d2227906297cd89c181489ba
BalajiJBcs <jbbalajics@gmail.com>
2018-03-12 23:34:11 +0530

    Math operation with functions

---
+++ MathOperation.py
@@ -0,0 +1,24 @@
+# Following code used to perform different mathmatical operations using python
+# @author : Balaji J.B
+#  </iLoveCS>
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b    
+
+def div(a,b):
+    return a/b
+
+if __name__ == '__main__' :
+    a = int(input("print the values :"))
+    b = int(input("enter your value :"))   
+
+    print(add(a,b))
+    print(sub(a,b))
+    print(mul(a,b))
+    print(div(a,b))
